d6410bd760a7031f49a5f5d6da189d917d914760
guid21 <uryuk.uz@gmail.com>
2021-06-05 19:34:43 +0200

    Completed task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    status: boolean;
 
-    constructor() { }
+    constructor() {
+        document.addEventListener('status', (event: any) => {
+            console.log(event.detail);
 
+            this.status = event.detail;
+        });
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {openDB} from "idb";
+import { Component, OnInit } from '@angular/core';
+import { openDB } from 'idb';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
     username: string = '';
-    dbPromise: any
-    oilFlag: boolean
-    constructor() { }
+    dbPromise: any;
+    oilFlag: boolean;
+    constructor() {}
 
     ngOnInit(): void {
         this.dbPromise = openDB('module-federation', 1, {
@@ -19,15 +18,16 @@ export class HomeComponent implements OnInit {
                 db.createObjectStore('host');
             },
         });
-        this.checkOilFlag()
+        this.checkOilFlag();
     }
 
     async checkOilFlag() {
-        (await this.dbPromise).get('host', 'oil').then((value: boolean) => this.oilFlag = value);
+        (await this.dbPromise)
+            .get('host', 'oil')
+            .then((value: boolean) => (this.oilFlag = value));
     }
 
     async setName() {
-        (await this.dbPromise).put('host', this.username, 'user')
+        (await this.dbPromise).put('host', this.username, 'user');
     }
-
 }
