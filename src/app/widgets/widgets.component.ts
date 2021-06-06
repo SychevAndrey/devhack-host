d1474d92a2c104a60cfc0e298c4f6d1fa6bb961c
Andrey Sychev <nukemoore@gmail.com>
2021-06-06 09:13:43 +0300

    fix: remove btns when widgets added

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Subject } from 'rxjs';
 import { LookupService } from '../lookup.service';
 import { RemoteComponentConfig } from '../remote-component-config';
@@ -13,11 +13,13 @@ export class WidgetsComponent implements OnInit {
     configs: RemoteComponentConfig[] = [];
     configs2: RemoteComponentConfig[] = [];
     synchronizer: Subject<RemoteComponentConfig> = new Subject();
+    oilAdded: boolean = false;
+    btcAdded: boolean = false;
 
     constructor(private lookupService: LookupService) { }
 
     ngOnInit(): void {
-        this.loadConfigs().then(() => console.log('MFE config loaded'));
+        this.loadConfigs();
     }
 
     async loadConfigs(): Promise<void> {
@@ -25,6 +27,11 @@ export class WidgetsComponent implements OnInit {
     }
 
     add(config: RemoteComponentConfig) {
+        if (config === this.configs[1]) {
+            this.oilAdded = true
+        } else {
+            this.btcAdded = true
+        }
         this.configs2.push(config);
     }
 
